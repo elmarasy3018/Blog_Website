4d7b5e20dae2755ca959c975f64fb64a13628bff
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2024-07-08 15:14:01 +0300

    Migration for translations table

--- database/migrations/2023_09_05_083354_create_posts_table.php
+++ database/migrations/2023_09_05_083354_create_posts_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->text('content');
             $table->integer('user_id');
             $table->timestamps();
         });
---
+++ database/migrations/2024_07_08_120812_create_post_translations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_translations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            // mandatory fields
+            $table->string('locale')->index();
+
+            // Foreign key to the main model
+            $table->unsignedInteger('post_id');
+            $table->unique(['post_id', 'locale']);
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+
+            // Actual fields you want to translate
+            $table->string('title');
+            $table->text('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_translations');
+    }
+};
